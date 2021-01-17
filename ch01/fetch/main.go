ea96f5bc6431a1a0ec4542c46f62b33187371504
costaxu <costaxu@tencent.com>
2021-01-17 14:31:36 +0800

    fetch

--- ch01/fetch/main.go
+++ ch01/fetch/main.go
@@ -3,24 +3,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	//"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		//b, err := ioutil.ReadAll(resp.Body)
+		//resp.Body.Close()
+		_, err = io.Copy(os.Stdin, resp.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		//fmt.Printf("%s", b)
 	}
 }
